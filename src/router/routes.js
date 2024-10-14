8ebb88c29fa303921bacf772832b0e26835c7d42
TheTrunk <tadeas.kmenta@zel.cash>
2024-10-14 21:18:01 +0800

    serve

--- src/router/routes.js
+++
@@ -1,4 +0,0 @@
-export const ROUTE_INDEX = '/';
-export const ROUTE_HOW_TO_INSTALL = '/how-to-install';
-export const ROUTE_PRIVACY_POLICY = '/privacy-policy';
-export const ROUTE_TERMS_OF_USE = '/terms-of-use';
--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-import { XWALLET_FILENAME } from '../constants/itemLinks';
-
-export const downloadFile = () => {
-  window.location.href = `${window.location.origin}/${XWALLET_FILENAME}`;
-};
